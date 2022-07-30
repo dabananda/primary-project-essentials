e20984895304bfaf984a146f075b4054cb8a6bd4
Dabananda Mitra <97220451+dabananda@users.noreply.github.com>
2022-07-30 18:09:51 +0600

    Add files via upload

---
+++ useNavigate-in-function.js
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+class MyBackButton extends React.Component {
+  render() {
+    // Get it from props
+    const { navigate } = this.props;
+  }
+}
+
+// Wrap and export
+export default function(props) {
+  const navigate = useNavigate();
+
+  return <MyBackButton {...props} navigate navigate />;
+}
